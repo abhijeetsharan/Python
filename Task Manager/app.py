251c8b9aa43fc5183ffd37f859dbfd0f75173498
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2025-02-11 02:02:07 +0530

    Added port in app.py file

--- Task Manager/app.py
+++ Task Manager/app.py
@@ -39,4 +39,5 @@ def delete_task(task_id):
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
